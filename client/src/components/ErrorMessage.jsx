17ad2cfbd62ec204ffe26288f6251465f0863175
mohammadprofessional <mohammadhunan@gmail.com>
2018-06-05 07:00:59 -0400

    login validation and error styling

---
+++ client/src/components/ErrorMessage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+let ErrorMessage = ({errortext}) => (
+    <div className="error-message">
+        <p>{errortext}</p>
+    </div>
+);
+
+
+export default ErrorMessage;
--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-// import { Button } from 'react-bootstrap';
+import ErrorMessage from './ErrorMessage';
+const emailErrorMessage = "Please enter a proper ID value, a proper ID value is name@emailprovider.extension, for instance: johndoe@protonmail.com";
+const passwordErrorMessage = "Please enter a proper password value, a proper password value is contains at least 8 characters, including one uppercase letter, one lowercase character, one number, and one special character";
 
 class LoginPage extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      loginErrors: []
     }
+  }
+  attemptLogin(){
+    let emailValidated = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.username.value); //if email is not in <word>@<domain>.<ext> format, emailValidated will return false
+    let passwordValidated = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/.test(this.password.value);
+
+
+    if(emailValidated && passwordValidated){
+      this.props.logIn();
+    }else if(emailValidated && !passwordValidated){
+      /* email is correct; password is incorrect */
+      this.setState({ loginErrors: [passwordErrorMessage] })
+    }else if(!emailValidated && passwordValidated){
+      /* email is incorrect; password is correct */
+      this.setState({ loginErrors: [emailErrorMessage] })
+    }else{
+      /* neither password or email is correct */
+      this.setState({ loginErrors: [emailErrorMessage,passwordErrorMessage] })
+    }
+    
   }
   render(){
     return(<div>
+      {
+        this.state.loginErrors.map((error) => {
+          return(<ErrorMessage errortext={error}/>)
+        })
+      }
       <form>
-        <label htmlFor="username">Username:</label>
-        <input id="username" type="text" placeholder="username" className="input-calm"/>
+        <label htmlFor="username">ID:</label>
+        <input 
+          ref={(element) => {this.username = element}}
+          id="username"
+          type="text"
+          placeholder="username"
+          className="input-calm"
+        />
 
         <label htmlFor="password">Password:</label>
-        <input id="password" type="text" placeholder="password" className="input-calm"/>
-
+        <input 
+          ref={(element) => {this.password = element}}
+          id="password"
+          type="password"
+          placeholder="password"
+          className="input-calm"/>
         
+        <button type="button" onClick={() => this.attemptLogin()}>Login</button>
       </form>
-      <button onClick={() => this.props.logIn()}>Login</button>
     </div>)
   }
 }
