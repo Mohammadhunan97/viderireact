6b4e9b07013d0775db4ba276754bf2015a76c531
mohammadprofessional <mohammadhunan@gmail.com>
2018-06-05 06:13:36 -0400

    login page basic form with minimal styling

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import { Button } from 'react-bootstrap';
 
-let LoginPage = ({ logIn }) => {
-  return(
-    <div>
-      <button onClick={() => logIn()}>Login</button>
-    </div>
-  )
+class LoginPage extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+  render(){
+    return(<div>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input id="username" type="text" placeholder="username" className="input-calm"/>
+
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="text" placeholder="password" className="input-calm"/>
+
+        
+      </form>
+      <button onClick={() => this.props.logIn()}>Login</button>
+    </div>)
+  }
 }
 
 export default LoginPage;
